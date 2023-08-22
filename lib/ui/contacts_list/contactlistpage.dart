d3cf9dfd335f4f4933b16900ce48b8290b077d84
Juwonlo <owoeyejuju@gmail.com>
2023-08-22 16:55:20 +0100

    My first commit

---
+++ lib/ui/contacts_list/contactlistpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class ContactListPage extends StatelessWidget {
+  const ContactListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+    );
+  }
+}
